cd97053effb760075efc65c1d04ae300b4884152
赵晓扬 <59006834+Goatherd0072@users.noreply.github.com>
2022-10-19 16:10:00 +0800

    添加了 Schlick Approximation，和 internal refract

--- main.cc
+++ main.cc
@@ -116,14 +116,14 @@ int main()
 
     //添加材质
     auto material_ground = make_shared<lambertian>(color3(0.8, 0.8, 0.0));
-    //auto material_center = make_shared<lambertian>(color3(0.1, 0.2, 0.5));
-    auto material_center = make_shared<dielectric>(1.5);
+    auto material_center = make_shared<lambertian>(color3(0.1, 0.2, 0.5));
     auto material_left = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<metal>(color3(0.8, 0.6, 0.2), 1.0);
+    auto material_right = make_shared<metal>(color3(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // 渲染
--- material.h
+++ material.h
@@ -68,6 +68,7 @@ public:
 };
 
 //Dielectrics 介质，模拟光传播过程中的介质，以实现玻璃、钻石等折射的效果
+// 当此介质的物体，半径为负数时，不会影响其几何形状，但由于法线向内，所以可以用作气泡来制作空心玻璃球
 class dielectric : public material
 {
 public:
@@ -80,55 +81,44 @@ public:
 		double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;//如果在外侧，根据折射定律，折射率则为倒数
 
 		vec3 unit_dir = unit_vec3(r_in.direction());
-		
-		vec3 refracted = refract(unit_dir, rec.normal, refraction_ratio);
+		//vec3 unit_n = unit_vec3(rec.normal);
 
-		scattered = ray(rec.p, refracted);
+		double cos_theta = fmin(dot(-unit_dir, rec.normal), 1.0);
+		double sin_theta = sqrt(1 - cos_theta * cos_theta);
 
+		bool can_Refraction = sin_theta * refraction_ratio > 1.0;//判断能不能进行折射
+		vec3 direction;
+
+		if (can_Refraction || Schlick_Approximation(cos_theta, refraction_ratio) > random_double())
+		{
+			//必须进行反射
+			direction = reflect(unit_dir, rec.normal);
+		}
+		else
+		{
+			//可以进行折射
+			direction = refract(unit_dir, rec.normal, refraction_ratio);
+		}
+
+		scattered = ray(rec.p, direction);
 		return true;
 	}
 
 public:
 	double ir;//折射率
+
+private:
+	//使用Schlick Approximation来模拟玻璃等随角度进行变化的折射率。 https://en.wikipedia.org/wiki/Schlick%27s_approximation 
+	// 折射率 r = R0 + (1 - R0) ( (1 - cos) )^5 
+	// R0 = ( (n1 - n2)/(n1 + n2) )^2 
+	//R0是光的反射系数的平行于法线， n1 n2为两侧介质的折射率， 由于图形学入射光线大部分情况都在空气，所以 n1 近似于 1
+	static double Schlick_Approximation(double cos, double index_ref)
+	{
+		auto R0 = (1 - index_ref) / (1 + index_ref);
+		R0 *= R0;
+		return R0 + (1 - R0) * pow((1 - cos), 5);
+	}
 	
 };
-
-//class dielectric : public material {
-//public:
-//	dielectric(double index_of_refraction) : ir(index_of_refraction) {}
-//
-//	virtual bool scatter(
-//		const ray& r_in, const hit_record& rec, color3& attenuation, ray& scattered
-//	) const override {
-//		attenuation = color3(1.0, 1.0, 1.0);
-//		double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
-//
-//		vec3 unit_direction = unit_vec3(r_in.direction());
-//		double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
-//		double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
-//
-//		bool cannot_refract = refraction_ratio * sin_theta > 1.0;
-//		vec3 direction;
-//
-//		if (cannot_refract || reflectance(cos_theta, refraction_ratio) > random_double())
-//			direction = reflect(unit_direction, rec.normal);
-//		else
-//			direction = refract(unit_direction, rec.normal, refraction_ratio);
-//
-//		scattered = ray(rec.p, direction);
-//		return true;
-//	}
-//
-//public:
-//	double ir; // Index of Refraction
-//
-//private:
-//	static double reflectance(double cosine, double ref_idx) {
-//		 Use Schlick's approximation for reflectance.
-//		auto r0 = (1 - ref_idx) / (1 + ref_idx);
-//		r0 = r0 * r0;
-//		return r0 + (1 - r0) * pow((1 - cosine), 5);
-//	}
-//};
 
 #endif 
